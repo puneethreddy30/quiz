ebd3475b0839dbfd39a66c235462344ba80010c4
puneethreddy30 <puneethreddy30@gmail.com>
2019-10-31 22:37:42 -0500

    4 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from './service/auth.service';
 
 declare var require: any; 
 @Component({
@@ -12,8 +14,28 @@ export class LoginComponent implements OnInit {
   passward : string;
   error:string;
   login:any;
-  constructor(private router?: Router) { }
-
+  form;
+  constructor(private fb: FormBuilder,
+    private auth: AuthService,private router?: Router) {
+    this.form = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+    }
+    
+  sendToken(token: string) {
+    localStorage.setItem("LoggedInUser", token)
+  }
+  getToken() {
+    return localStorage.getItem("LoggedInUser")
+  }
+  isLoggednIn() {
+    return this.getToken() !== null;
+  }
+  logout() {
+    localStorage.removeItem("LoggedInUser");
+    this.router.navigate(["login"]);
+  }
   ngOnInit() {
     
 this.login = require('../../data/user.json');
@@ -21,8 +43,13 @@ console.log("login", this.login);
   }
 
   welcomeMe(uname:string,pwd:string){
+
     this.name = uname;
     this.passward = pwd;
+    if (this.form.valid) {
+      this.auth.sendToken(this.form.value.email)
+      this.router.navigate(["home"]);
+    }
     for(let data in this.login) {
       console.log(this.name);
       console.log("login ", this.login[data].name);
--- src/app/services/auth.gaurd.ts
+++ src/app/services/auth.gaurd.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
-import { AuthService } from './auth.service';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 @Injectable()
-export class AuthGuardService implements CanActivate {
-  constructor(public auth: AuthService, public router: Router) {}
-  canActivate(): boolean {
-    if (!this.auth.isAuthenticated()) {
-      this.router.navigate(['login']);
-      return false;
-    }
+export class AuthGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     return true;
   }
+}
